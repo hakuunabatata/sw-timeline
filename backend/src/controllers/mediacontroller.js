e154658ae2510eaa00206285b93055a7d096893d
hakuunabatata <emachadomartins1999@gmail.com>
2020-04-19 15:13:40 -0300

    filters

--- backend/src/controllers/mediacontroller.js
+++ backend/src/controllers/mediacontroller.js
@@ -1,16 +1,15 @@
 const Media = require("../models/media");
 
-
 module.exports = {
-    async index (req, res)  {
-        const filter = req.headers.filter
+  async index(req, res) {
+    const filter = JSON.parse(req.headers.authorization);
 
-        const media = await Media.find(filter)
-        return res.json(media)
-    },
-    async create(req, res)  {
-        const media = await Media.create(req.body);
-    
-        return res.json(media);
-    }
-}
+    const media = await Media.find(filter).sort({ timeline: 1 });
+    return res.json(media);
+  },
+  async create(req, res) {
+    const media = await Media.create(req.body);
+
+    return res.json(media);
+  },
+};
--- frontend/src/pages/MainPage/index.js
+++ frontend/src/pages/MainPage/index.js
@@ -7,28 +7,44 @@ import {
   FiTv,
   FiBookOpen,
   FiFilm,
+  FiHash,
+  FiPlus,
+  FiAtSign,
 } from "react-icons/fi";
 import api from "../../services/api";
 
 export default function MainPage() {
   const [medias, setMedia] = useState([]);
+  const [filter, setFilter] = useState("{}");
+  const types = [
+    "all",
+    "film",
+    "tv",
+    "novel",
+    "game",
+    "comic",
+    "canon",
+    "legends",
+  ];
 
-
-  function setIcon(media) {
-    if (media === "game") {
-      return <FiPlay size={50} />;
-    }
-    if (media === "tv") {
-      return <FiTv size={50} />;
-    }
-    if (media === "novel") {
-      return <FiBookOpen size={50} />;
-    }
-    if (media === "comic") {
-      return <FiMessageCircle size={50} />;
-    }
-    if (media === "film") {
-      return <FiFilm size={50} />;
+  function setIcon(type) {
+    switch (type) {
+      case "all":
+        return <FiAtSign size={50} />;
+      case "game":
+        return <FiPlay size={50} />;
+      case "tv":
+        return <FiTv size={50} />;
+      case "novel":
+        return <FiBookOpen size={50} />;
+      case "comic":
+        return <FiMessageCircle size={50} />;
+      case "film":
+        return <FiFilm size={50} />;
+      case "legends":
+        return <FiHash size={50} />;
+      case "canon":
+        return <FiPlus size={50} />;
     }
   }
 
@@ -42,30 +58,59 @@ export default function MainPage() {
   }
 
   useEffect(() => {
-    api.get("http://localhost:3300/infos").then((res) => {
-      setMedia(res.data);
-    });
-  }, []);
+    api
+      .get("http://localhost:3300/infos", {
+        headers: {
+          Authorization: filter,
+        },
+      })
+      .then((res) => {
+        setMedia(res.data);
+      });
+  }, [filter]);
 
   return (
     <div id="container">
       <div className="logo">
         <img src={Logo} alt="Star Wars Logo" />
       </div>
-
       <ul>
+        <li className="btncontainer">
+          {types.map((type) => (
+            
+            <button
+              className={`filterbtn ${type}`}
+              onClick={() => {
+                if (type === "all") {
+                  setFilter("{}");
+                } else {
+                  var index = "";
+                  if (["canon", "legends"].includes(type)) {
+                    index = "universe";
+                  } else {
+                    index = "media";
+                  }
+                  setFilter(`{"${index}":"${type}"}`);
+                }
+              }}
+            >
+              <div className="btnicon">{setIcon(type)}</div>
+              <div className="btntxt">{type}</div>
+            </button>
+          ))}
+        </li>
         {medias.map((media) => (
-          <li key={media._id} className={media.media + " " + media.universe}>
+          <li key={media._id} className="content">
             <img src={media.image} alt="" />
             <div className="title">{media.title}</div>
-            <div className="type">
+            <div className={`type ${media.media}`}>
               <div className="icon">{setIcon(media.media)}</div>
               <div className="typetxt">{media.media}</div>
             </div>
             <div className="timeline">{setTimeline(media.timeline)}</div>
             <div className="series">{media.series}</div>
             <div className="date">{media.releasedate}</div>
-            <div className="universe">{media.universe}</div>
+            <div className={`universe ${media.universe}`}>{media.universe}</div>
           </li>
         ))}
       </ul>
